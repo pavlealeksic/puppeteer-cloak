719452c3e4d22af61a33be0dfc82577b61d13082
Pavle Aleksic <pavlealeksic@live.com>
2020-05-14 19:55:29 +0200

    package fix

--- src/index.js
+++ src/index.js
@@ -6,37 +6,35 @@
  */
 module.exports = function(page) {
   page.evaluateOnNewDocument(() => {
-    Object.defineProperty(navigator, 'webdriver', {
-      get: () => false,
-    });
+    Object.defineProperty(navigator, 'webdriver', {get: () => false, });
     Object.defineProperty(window, 'onbeforeunload', {
       configurable: false,
       writeable: false,
       value: function () { }
-    })
+    });
     Object.defineProperty(window, 'onunload', {
       configurable: false,
       writeable: false,
       value: function () { }
-    })
+    });
 
     if (!window.chrome) {
-      const installer = { install() { } }
+      const installer = { install() { } };
       window.chrome = {
         app: { isInstalled: false },
         webstore: {
           onInstallStageChanged: {},
           onDownloadProgress: {},
           install(url, onSuccess, onFailure) {
-            installer.install(url, onSuccess, onFailure)
+            installer.install(url, onSuccess, onFailure);
           }
         },
         csi() { },
         loadTimes() { }
-      }
+      };
     }
 
-    if (!window.chrome.runtime) {
+    if (!window.chrome.runtime) 
       window.chrome.runtime = {
         PlatformOs: {
           MAC: 'mac',
@@ -78,36 +76,28 @@ module.exports = function(page) {
         },
         connect: function () { }.bind(function () { }),
         sendMessage: function () { }.bind(function () { })
-      }
-    }
+      };
+    
 
-    if (HTMLIFrameElement.prototype.__lookupGetter__('contentWindow') == null) {
+    if (HTMLIFrameElement.prototype.__lookupGetter__('contentWindow') == null) 
       Object.defineProperty(HTMLIFrameElement.prototype, 'contentWindow', {
         get: function () {
-          return window
+          return window;
         }
-      })
-    }
+      });
+    
 
-    if ((navigator.plugins || []).length === 0) {
-      Object.defineProperty(navigator, 'plugins', {
-        get: () => [1, 2, 3, 4, 5]
-      })
-    }
+    if ((navigator.plugins || []).length === 0) 
+      Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]});
+    
 
-    if ((navigator.languages || []).length === 0) {
-      Object.defineProperty(navigator, 'languages', {
-        get: () => ['en-US', 'en']
-      })
-    }
+    if ((navigator.languages || []).length === 0) 
+      Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']});
+    
     const originalQuery = window.navigator.permissions.query;
     return window.navigator.permissions.query = (parameters) => (
-      parameters.name === 'notifications' ?
-        Promise.resolve({
-          state: Notification.permission
-        }) :
-        originalQuery(parameters)
+      parameters.name === 'notifications' ?        Promise.resolve({state: Notification.permission}) :        originalQuery(parameters)
     );
-  })
+  });
   return page;
 };
